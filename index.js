9a6dc370bfdb70b25b546bacd262f413836c85fd
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-07-31 13:21:42 +0300

    generateKey

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+//Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
+const generateKey = (length, possible)  => {
+    const characters =  possible.length;
+    let key = "";
+    for (let i = 0; i < length; i++) {
+        const random = Math.floor(Math.random() * characters);
+        key += possible[random];
+    }
+   return key;
+}
+console.log(key);
